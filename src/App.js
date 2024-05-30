b9e3b6f9c58026db470cfcb24ecd36220e9af902
heet003 <heet.b003@gmail.com>
2024-05-30 18:30:52 +0530

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [displayText, setDisplayText] = useState("0");
+
+  const handleClick = (event) => {
+    let value = "";
+    value += event.target.textContent;
+    setDisplayText(value);
+    console.log(event.target.textContent);
+  };
+
   return (
     <div className="App">
       <div className="cal-box">
-        <div className="display">0</div>
+        <div className="display">{displayText}</div>
         <div className="controls">
           {/* 1 */}
           <div>
@@ -24,31 +34,31 @@ function App() {
           </div>
           {/* 3 */}
           <div>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>×</button>
+            <button onClick={handleClick}>7</button>
+            <button onClick={handleClick}>8</button>
+            <button onClick={handleClick}>9</button>
+            <button onClick={handleClick}>×</button>
           </div>
           {/* 4 */}
           <div>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>−</button>
+            <button onClick={handleClick}>4</button>
+            <button onClick={handleClick}>5</button>
+            <button onClick={handleClick}>6</button>
+            <button onClick={handleClick}>−</button>
           </div>
           {/* 5 */}
           <div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
+            <button onClick={handleClick}>1</button>
+            <button onClick={handleClick}>2</button>
+            <button onClick={handleClick}>3</button>
+            <button onClick={handleClick}>+</button>
           </div>
           {/* 6 */}
           <div>
             <button>±</button>
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
+            <button onClick={handleClick}>0</button>
+            <button onClick={handleClick}>.</button>
+            <button className="submit">=</button>
           </div>
         </div>
       </div>
