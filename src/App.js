23003826706bcea942d33b0e281f049feba2474f
heet003 <heet.b003@gmail.com>
2024-05-30 18:12:00 +0530

    new file:   public/clear.png 	deleted:    public/favicon.ico 	modified:   public/index.html 	new file:   public/key.png 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="cal-box">
+        <div className="display">0</div>
+        <div className="controls">
+          {/* 1 */}
+          <div>
+            <button>%</button>
+            <button>CE</button>
+            <button>C</button>
+            <button>
+              <img src="./clear.png" alt="Clear"></img>
+            </button>
+          </div>
+          {/* 2 */}
+          <div>
+            <button>⅟x</button>
+            <button>x²</button>
+            <button>√x</button>
+            <button>÷</button>
+          </div>
+          {/* 3 */}
+          <div>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>×</button>
+          </div>
+          {/* 4 */}
+          <div>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>−</button>
+          </div>
+          {/* 5 */}
+          <div>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+          </div>
+          {/* 6 */}
+          <div>
+            <button>±</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
